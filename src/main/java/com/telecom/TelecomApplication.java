76254e53cbdf95fad772b9c980d802c1662d16e9
SalahuddinShayan <s.m.shayan04@gmail.com>
2022-10-08 08:39:13 +0530

    revert

--- src/main/java/com/telecom/TelecomApplication.java
+++ src/main/java/com/telecom/TelecomApplication.java
@@ -2,9 +2,10 @@ package com.telecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TelecomApplication {
+public class TelecomApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TelecomApplication.class, args);
